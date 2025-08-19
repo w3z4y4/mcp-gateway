01974408f52f010e047bf13066c474d8a0653b50
王泽洋 <876989946@qq.com>
2025-08-20 02:16:10 +0800

    webfilter about auth

--- demo/mcp-client-demo/src/main/java/org/jdt/mcp/gateway/demo/ctl/ChatController.java
+++ demo/mcp-client-demo/src/main/java/org/jdt/mcp/gateway/demo/ctl/ChatController.java
@@ -1,7 +1,5 @@
 package org.jdt.mcp.gateway.demo.ctl;
 
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jdt.mcp.gateway.demo.service.ChatService;
 import org.springframework.web.bind.annotation.RequestMapping;
--- demo/mcp-server-demo/src/main/java/org/jdt/mcp/gateway/demo/service/DemoService.java
+++ demo/mcp-server-demo/src/main/java/org/jdt/mcp/gateway/demo/service/DemoService.java
@@ -2,19 +2,20 @@ package org.jdt.mcp.gateway.demo.service;
 
 
 import entity.Employee;
+import lombok.extern.slf4j.Slf4j;
+import org.jdt.mcp.gateway.demo.conf.AuthKeyContext;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.ai.tool.annotation.ToolParam;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class DemoService {
-//    @Tool(description = "根据姓名查询工作单位")
-//    public String getJobs(@ToolParam(description = "姓名") String name){
-//        return name+"在工商银行软件开发中心工作";
-//    }
+
 
     @Tool(description = "根据姓名和手机号查询工作单位")
     public String getWorks(@ToolParam(description = "输入姓名和手机号") Employee employee){
         return employee.getName()+"在工商银行软件开发中心工作";
     }
+
 }
